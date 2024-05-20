c1a6aa125663a5e2b93e6e8bfb1c05321f15e3d7
Goecha <goecha.ksh@gmail.com>
2024-05-20 13:21:45 +0530

    url-updated

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ const NewCollections = () => {
   const [new_collection, setNew_collection] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://manipurmart-backend.onrender.com/newcollections')
       .then((response) => response.json())
       .then((data) => setNew_collection(data));
   }, [])
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -8,7 +8,7 @@ const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/popularproducts')
+    fetch('https://manipurmart-backend.onrender.com/popularproducts')
       .then((response) => response.json())
       .then((data) => setPopularProducts(data));
   }, [])
